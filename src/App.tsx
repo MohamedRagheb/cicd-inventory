136ce8baba959f77ec88d3c5599f16c9414afc20
MohamedRagheb <136890757+MRaghebdev@users.noreply.github.com>
2025-11-07 19:13:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div>hello from inventory test {4 - 2}</div>;
+  return <div>hello from inventory test test {4 - 2}</div>;
 }
 
 export default App;
